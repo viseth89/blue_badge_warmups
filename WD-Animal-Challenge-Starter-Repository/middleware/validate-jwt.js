18c95de2be263d17b46c090199e6fb4eb87e578e
datalogics-vsen <vsen@datalogics.com>
2021-06-15 17:42:16 -0500

    codealong with Justin

---
+++ WD-Animal-Challenge-Starter-Repository/middleware/validate-jwt.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../models');
+
+const validateJWT = async(req, res, nest) => {
+    if(req.method == "OPTIONS"){
+        next();
+    } else if(
+        req.headers.authorization &&
+        req.headers.authorization.includes('Bearer')
+    ){
+        const { authorization } = req.headers;
+        console.log("authorization -->", authorizsation )
+
+        const payload = authorization ? jwt.verify (
+            authorization.includes("Bearer")
+                ? authorization.split (" ")[1]
+                : authorization, process.env.JWT_SECRET
+        )
+        : undefined;
+        console.log("payload -->", payload)
+
+        if(payload){
+            let foundUser = await User.findOne({
+                where: {
+                    id: payload.id
+                }
+            })
+            console.log("foundUser-->", founderUser)
+            
+        }
+    }
+}
+
+modules.exports = validateJWT;
