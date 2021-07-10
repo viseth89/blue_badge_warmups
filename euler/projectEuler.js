3b3aefc7fc4857f5809894350cc7c67ce8bbdac2
viseth89 <viseth89@gmail.com>
2021-07-10 15:22:15 -0500

    Trying to understand solution

--- euler/projectEuler.js
+++ euler/projectEuler.js
@@ -6,9 +6,11 @@ Find the sum of all hte multiples of 3 or 5 below 1000
 
 let solve = (num) => {
     total = 0
-    for(let i = 1; i<11; i++){
+    for(let i = 1; i<11; i++)
 
-    }
+        if(i %3 == 0 || i % 5 == 0) 
+            total +=1
+    
     return total   
 }
 
