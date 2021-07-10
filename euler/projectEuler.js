946d7c44fc3463bbb3e86fccf77528d617f70a48
viseth89 <viseth89@gmail.com>
2021-07-10 15:20:46 -0500

    Working along with ProjectEuler trying to solve problem

---
+++ euler/projectEuler.js
@@ -0,0 +1,16 @@
+/* 
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we 3, 5, 6 and 9.  The sume of these multiples is 23.
+
+Find the sum of all hte multiples of 3 or 5 below 1000
+*/
+
+let solve = (num) => {
+    total = 0
+    for(let i = 1; i<11; i++){
+
+    }
+    return total   
+}
+
+console.log(solve(10) == 23)
+console.log(solve(100))
